eb156d2e4b9afa9b814d54992f6e355cfacce09f
lioda <parker.lioda@gmail.com>
2016-05-29 09:07:39 +0200

    First version of Polynomial.toDigits()

--- src/main/java/org/lioda/radix/Polynomial.java
+++ src/main/java/org/lioda/radix/Polynomial.java
@@ -1,7 +1,11 @@
 package org.lioda.radix;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/** Plynomial representation of a number, with any radix. */
 public class Polynomial {
 
     private final BigInteger number;
@@ -12,5 +16,20 @@ public class Polynomial {
 
     public String toString(int radix) {
         return number.toString(radix);
+    }
+
+
+    public Iterable<Integer> toDigits(int radix) {
+        BigInteger rad = new BigInteger(String.valueOf(radix));
+        List<Integer> result = new ArrayList<Integer>();
+        BigInteger q = number;
+        int i = 0;
+        for (;q.compareTo(BigInteger.ZERO) == 1;++i) {
+            BigInteger r = q.mod(rad);
+            q = q.divide(rad);
+            result.add(r.intValue());
+        }
+        Collections.reverse(result);
+        return result;
     }
 }
--- src/test/java/org/lioda/radix/PolynomialTest.java
+++ src/test/java/org/lioda/radix/PolynomialTest.java
@@ -1,10 +1,8 @@
 package org.lioda.radix;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PolynomialTest {
@@ -13,6 +11,12 @@ public class PolynomialTest {
     public void polynomialStringifyInAnyRadix() {
         Polynomial polynomial = new Polynomial("123",10);
         assertThat(polynomial.toString(16), is("7b"));
+    }
+
+    @Test
+    public void polynomialToDigitsReturnsIterableOfDigits() {
+        Polynomial polynomial = new Polynomial("123",10);
+        assertThat(polynomial.toDigits(16), contains(7, 11));
     }
 
 }

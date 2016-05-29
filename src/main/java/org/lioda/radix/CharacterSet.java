94a2258b31e93233a2e337b110b8085d3f56fd11
lioda <parker.lioda@gmail.com>
2016-05-29 22:31:16 +0200

    Create CharacterSet

---
+++ src/main/java/org/lioda/radix/CharacterSet.java
@@ -0,0 +1,55 @@
+package org.lioda.radix;
+
+import java.util.*;
+
+/**
+ * Set of characters.
+ */
+public class CharacterSet implements Iterable<Character> {
+    public static CharacterSet fromCharacters(Character... chars) {
+        return new CharacterSet(Arrays.asList(chars));
+    }
+
+    public static CharacterSet fromRange(char first, char last) {
+        return new CharacterSet(listFromRange(first, last));
+    }
+
+    private static ArrayList<Character> listFromRange(char first, char last) {
+        ArrayList<Character> characters = new ArrayList<Character>();
+        for (char c = first ; c <= last ; ++c) {
+            characters.add(c);
+        }
+        return characters;
+    }
+
+    //////////// End of constructors
+
+    private final List<Character> list;
+    private CharacterSet(List<Character> chars) {
+        list = Collections.unmodifiableList(chars);
+    }
+
+    @Override
+    public Iterator<Character> iterator() {
+        return list.iterator();
+    }
+
+    public CharacterSet addRange(char first, char last) {
+        return new CharacterSet(addToList(listFromRange(first,last)));
+    }
+
+    public CharacterSet addCharacters(Character... chars) {
+        return new CharacterSet(addToList(Arrays.asList(chars)));
+    }
+
+    private List<Character> addToList(List<Character> other) {
+        List<Character> result = new ArrayList<Character>();
+        result.addAll(list);
+        result.addAll(other);
+        return result;
+    }
+
+    public char get(int i) {
+        return list.get(i);
+    }
+}
---
+++ src/test/java/org/lioda/radix/CharacterSetTest.java
@@ -0,0 +1,48 @@
+package org.lioda.radix;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CharacterSetTest {
+    @Test
+    public void createFromCharacterList() {
+        assertThat(CharacterSet.fromCharacters('a','b','c'), contains('a','b','c'));
+    }
+
+    @Test
+    public void canAccessItemByIndex() {
+        assertThat(CharacterSet.fromRange('0', '9').get(2), is('2'));
+    }
+
+    @Test
+    public void createFromRange() {
+        assertThat(CharacterSet.fromRange('0', '9'), contains('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+    }
+
+    @Test
+    public void canAddRange() {
+        assertThat(CharacterSet.fromRange('0', '2').addRange('a', 'c'), contains('0', '1', '2', 'a', 'b', 'c'));
+    }
+
+    @Test
+    public void canAddCharacterList() {
+        assertThat(CharacterSet.fromRange('0', '2').addCharacters('a', 'c'), contains('0', '1', '2', 'a', 'c'));
+    }
+
+    @Test
+    public void characterSetIsImmutable() {
+        CharacterSet set = CharacterSet.fromRange('0', '2');
+        assertThat(set.addRange('a', 'c'), not(sameInstance(set)));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void cannotRemoveElement() {
+        Iterator<Character> iterator = CharacterSet.fromRange('0', '2').iterator();
+        iterator.next();
+        iterator.remove();
+    }
+}
